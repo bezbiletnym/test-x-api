cb70f99a3d6a2428b0fdf079db9ef384be891422
m.omelyanenko <61245298+bezbiletnym@users.noreply.github.com>
2025-03-24 15:56:35 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# X API: like a tweet by ID
+
+A test project
+
+## Installation:
+### Use the following commands in your terminal:
+
+#### Check pip installation:
+```
+python -m ensurepip
+```
+
+#### *Optional (if you want to use venv):*
+```
+python -m venv venv
+.\venv\Scripts\activate
+```
+
+#### Install required packages:
+```
+pip install -r requirements.txt
+```
+
+Create and configure '.env' file in the project directory. Example file contents:
+```
+CONSUMER_KEY='blabla1234'
+CONSUMER_SECRET='evenmorebla999'
+ACCESS_TOKEN='111-blablabla'
+ACCESS_TOKEN_SECRET='bla123bla'
+```
+
+## Run:
+
+```
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,43 @@
+import tweepy, dotenv, os, time
+
+def get_reset_time_from_headers(headers: dict) -> str:
+    epoch_reset_time = headers.get('x-rate-limit-reset')
+    reset_time = 'Unknown'
+    if epoch_reset_time:
+        reset_time = time.strftime('%d-%m-%Y %H:%M:%S',
+                                   time.gmtime(float(epoch_reset_time)))
+        reset_time += ' GMT'
+    return reset_time
+
+# Loading .env file
+dotenv.load_dotenv()
+
+#OAuth 1.0a
+client = tweepy.Client(consumer_key=os.getenv('CONSUMER_KEY'),
+                       consumer_secret=os.getenv('CONSUMER_SECRET'),
+                       access_token=os.getenv('ACCESS_TOKEN'),
+                       access_token_secret=os.getenv('ACCESS_TOKEN_SECRET'))
+
+tweet_id = input("Type in an ID of a tweet you want to like: ")
+
+try:
+    print("Sending a request to the API...")
+    response = client.like(tweet_id=tweet_id, user_auth=True)
+    if response.data.get("liked"):
+        print(f"Tweet {tweet_id} is liked successfully")
+except tweepy.errors.TwitterServerError as err:
+    print(f"Error: Something wrong with API. Code: {err.response.status_code}")
+except tweepy.errors.HTTPException as err:
+    print(f"Error: {err.response.reason}")
+    status_code = err.response.status_code
+    match status_code:
+        case 429:
+            print("Rate limit exceeded")
+            print(f"Reset at: {get_reset_time_from_headers(err.response.headers)}")
+        case 401:
+            print("Failed to authorize. Please check your tokens")
+except TypeError as err:
+    print(f"Error: {repr(err)}.")
+    print("Please check .env file")
+except Exception as err:
+    print(f"Error: {repr(err)}")
